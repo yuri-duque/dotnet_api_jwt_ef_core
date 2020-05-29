25d5ecb5701a2494c6c6abf8c5d223cfe7350a5b
Yuri Duque <yuri.duque@hotmail.com>
2020-05-28 22:54:05 -0300

    criando projeto e enviando o projeto com ef core

---
+++ Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("tb_usuario")]
+    public class Usuario
+    {
+        [Key]
+        [Required]
+        public long Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        public static void Map(ModelBuilder modelBuilder)
+        {
+            var map = modelBuilder.Entity<Usuario>();
+            map.Property(x => x.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ Repository/Context/BaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Repository.Context
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions<BaseContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            Usuario.Map(modelBuilder);
+        }
+
+        public DbSet<Usuario> usuarios { get; set; }
+    }
+}
